dfe301027f0ffd93fa81365e0f098f73d0a004ae
Molano <molano@ibp10333.csc.es>
2019-01-31 16:47:50 +0100

    remove prints

--- dualtask/data.py
+++ dualtask/data.py
@@ -41,8 +41,6 @@ def get_inputs_outputs(n_batch, n_time, n_bits, gng_time):
 
 
 def get_stims(stim, n_batch):
-    print(stim.shape)
-    print(n_batch)
     choice = np.random.choice(stim.shape[0], size=(n_batch, ))
     stim_seq = stim[choice].astype(int)
     return stim_seq, choice
