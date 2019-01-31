7ec68b612686f13db3eaa6484146a04d65c622ab
Molano <molano@ibp10333.csc.es>
2019-01-31 16:16:14 +0100

    minor changes

--- dualtask/run_DualTask.py
+++ dualtask/run_DualTask.py
@@ -12,16 +12,15 @@ from __future__ import print_function
 import pdb
 import sys
 import numpy as np
-import tensorflow as tf
 
 PATH_TO_FIXED_POINT_FINDER = '../'
 sys.path.insert(0, PATH_TO_FIXED_POINT_FINDER)
-from FlipFlop import FlipFlop
+from DualTask import DualTask
 from FixedPointFinder import FixedPointFinder
 from FixedPoints import FixedPoints
 
 # *****************************************************************************
-# STEP 1: Train an RNN to solve the N-bit memory task *************************
+# STEP 1: Train an RNN to solve the dual task *********************************
 # *****************************************************************************
 
 # Hyperparameters for AdaptiveLearningRate
@@ -39,22 +38,21 @@ hps = {
         'n_batch': 512,
         'n_time': 64,
         'n_bits': 3,
-        'p_flip': 0.5},
+        'noise': 0.1},
     'alr_hps': alr_hps
     }
 
-ff = FlipFlop(**hps)
-ff.train()
+dt = DualTask(**hps)
+dt.train()
 
 # Get example state trajectories from the network
 # Visualize inputs, outputs, and RNN predictions from example trials
-example_trials = ff.generate_flipflop_trials()
-ff.plot_trials(example_trials)
+example_trials = dt.generate_flipflop_trials()
+dt.plot_trials(example_trials)
 
 # *****************************************************************************
 # STEP 2: Find, analyze, and visualize the fixed points of the trained RNN ****
 # *****************************************************************************
-
 
 
 '''Initial states are sampled from states observed during realistic behavior
@@ -63,31 +61,31 @@ from one stable state to another, observed networks states spend little if any
 time near the unstable fixed points. In order to identify ALL fixed points,
 noise must be added to the initial states before handing them to the fixed
 point finder.'''
-NOISE_SCALE = 0.5 # Standard deviation of noise added to initial states
-N_INITS = 1024 # The number of initial states to provide
+NOISE_SCALE = 0.5  # Standard deviation of noise added to initial states
+N_INITS = 1024  # The number of initial states to provide
 
-n_bits = ff.hps.data_hps['n_bits']
-is_lstm = ff.hps.rnn_type == 'lstm'
+n_bits = dt.hps.data_hps['n_bits']
+is_lstm = dt.hps.rnn_type == 'lstm'
 
 '''Fixed point finder hyperparameters. See FixedPointFinder.py for detailed
 descriptions of available hyperparameters.'''
 fpf_hps = {}
 
 # Setup the fixed point finder
-fpf = FixedPointFinder(ff.rnn_cell,
-                       ff.session,
+fpf = FixedPointFinder(dt.rnn_cell,
+                       dt.session,
                        **fpf_hps)
 
 # Study the system in the absence of input pulses (e.g., all inputs are 0)
-inputs = np.zeros([1,n_bits])
+inputs = np.zeros([1, n_bits])
 
 '''Draw random, noise corrupted samples of those state trajectories
 to use as initial states for the fixed point optimizations.'''
-example_predictions = ff.predict(example_trials,
+example_predictions = dt.predict(example_trials,
                                  do_predict_full_LSTM_state=is_lstm)
 initial_states = fpf.sample_states(example_predictions['state'],
                                    n_inits=N_INITS,
-                                   rng=ff.rng,
+                                   rng=dt.rng,
                                    noise_scale=NOISE_SCALE)
 
 # Run the fixed point finder
@@ -96,8 +94,8 @@ unique_fps, all_fps = fpf.find_fixed_points(initial_states, inputs)
 # Visualize identified fixed points with overlaid RNN state trajectories
 # All visualized in the 3D PCA space fit the the example RNN states.
 unique_fps.plot(example_predictions['state'],
-    plot_batch_idx=range(30),
-    plot_start_time=10)
+                plot_batch_idx=range(30),
+                plot_start_time=10)
 
 print('Entering debug mode to allow interaction with objects and figures.')
 pdb.set_trace()
